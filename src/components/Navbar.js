fec8daf7141ada1ad5eb981c32ac735d020fcb25
Aditya_Mishra <adityasaiyansuper@gmail.com>
2023-04-29 22:09:12 +0530

    bug fixes, Alumni, Contact, Event Page(individual) added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,12 +4,12 @@ const Navbar = () => {
     return (
         <nav className="navbar">
             <div className="nav-container">
-                <li className='home-nav' ><NavLink exact activeClassName="active" to="/home">Home</NavLink></li> 
-                <li className='about-nav'><NavLink exact activeClassName="active" to="/about" >About</NavLink></li>
-                <li className='projects-nav'><NavLink exact activeClassName="active" to="/project">Projects</NavLink></li>
-                <li className='events-nav'><NavLink  exact activeClassName="active" to="/event">Events</NavLink></li>
-                <li className='contact-nav'><NavLink exact activeClassName="active" to="/contact">Contact Us</NavLink></li>
-                <li className='alumini-nav'><NavLink exact activeClassName="active" to="/alumni">Alumni</NavLink></li>
+                <li className='home-nav' ><NavLink exact activeclassname="active" to="/home">Home</NavLink></li> 
+                <li className='about-nav'><NavLink exact activeclassname="active" to="/about" >About</NavLink></li>
+                <li className='projects-nav'><NavLink exact activeclassname="active" to="/project">Projects</NavLink></li>
+                <li className='events-nav'><NavLink  exact activeclassname="active" to="/event">Events</NavLink></li>
+                <li className='contact-nav'><NavLink exact activeclassname="active" to="/contact">Contact Us</NavLink></li>
+                <li className='alumini-nav'><NavLink exact activeclassname="active" to="/alumni">Alumni</NavLink></li>
             </div>
         </nav>
     )
