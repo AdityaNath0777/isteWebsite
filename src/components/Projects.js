c04f85332b89734146079a7e2976818ef6d5894c
Divya <elataylor2002@gmail.com>
2023-04-30 12:28:25 +0530

    changed projects to subchapters on home page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,22 +2,26 @@ export const ProjectsProto = () => {
     return (
         <div className="projects">
             <div className="projects-header">
-                <h1>Projects</h1>
+                <h1>Student Interests Groups (SIG)</h1>
                 <p>Here are some projects that we have worked on:</p>
                 <span className="viewMore">View More</span>
             </div>
             <div className="project-container">
                 <div className="project">
-                    <h2>Project 1</h2>
-                    <p> <strong> Q-Fly Agent</strong>, a <em>smart AI enabled Drone</em> which delivers the courier on the customer's doorstep on time having 99.67% accuracy and improving each day.</p>
+                    <h2>ELECTRONIKA</h2>
+                    <p>This SIG controls all the electrical and robotics related events. It has successfully conducted many workshops on Drone making, home automation and self driving cars. Student members of Electronica have won various Hackathon and competitions like Technothlon.</p>
                 </div>
                 <div className="project">
-                    <h2>Project 2</h2>
-                    <p> <strong> PaiseBachao</strong>, a useful chrome extension used for <em>comparing past and present prices</em> of the products available on e-commerce and alerts you when price gets low.</p>
+                    <h2>SOFTECH</h2>
+                    <p>All coding related events are hosted by this SIG. It has conducted workshops on web development and machine learning and plans to continue with the tradition. </p>
                 </div>
                 <div className="project">
-                    <h2>Project 3</h2>
-                    <p> <strong> Enjoy Buddy</strong>, an <em>AI enabled Mental Health</em> product that enables/helps you enjoy every bit of life and also assits to tackle depression and lonliness. </p>
+                    <h2>SAHITYE</h2>
+                    <p>The literary SIG of ISTE. This is for all the closeted poets and writers. Here we provide a platform for all the non-technical ideas that your mind can fathom. We organise workshops on public speaking and other literary skills.</p>
+                </div>
+                <div className="project">
+                    <h2>PIXEL CRAFT</h2>
+                    <p>This SIG deals with all the designing work of the society. This SIG is behind all the eye-catching posters and stories that we see on ISTE's social media handles. One  can say that this SIG works as an ornament to the society.</p>
                 </div>
             </div>
         </div>
